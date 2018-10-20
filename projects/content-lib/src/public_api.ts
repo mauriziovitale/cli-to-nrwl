1bac711a926eb7354a1924e8e52ffb5a71b2c8c0
Marizio Vitale <maurizio.vitale@alfresco.com>
2018-10-20 14:52:55 +0100

    Move content-lib to libs and app tu apps

--- projects/content-lib/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of content-lib
- */
-
-export * from './lib/content-lib.service';
-export * from './lib/content-lib.component';
-export * from './lib/content-lib.module';
--- projects/process-lib/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of process-lib
- */
-
-export * from './lib/process-lib.service';
-export * from './lib/process-lib.component';
-export * from './lib/process-lib.module';
